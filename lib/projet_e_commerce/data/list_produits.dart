0885fa165d1cba80b03178b22fb12529ef9efdee
Mohamed Ben Jazia <mohamed.benjazia@polytechnicien.tn>
2026-03-06 08:52:59 +0100

    Suite Atelier 05: Finaliser liste des Produits

--- lib/projet_e_commerce/data/list_produits.dart
+++ lib/projet_e_commerce/data/list_produits.dart
@@ -1,8 +1,7 @@
-
 import 'package:projet1/projet_e_commerce/model/class_produit.dart';
 
 class AllProductData {
-  final List<Produit> _produits = [
+  static List<Produit> _produits = [
     Produit(
       id: 'Samsung1',
       title: 'Samsung Galaxy S9',
@@ -583,7 +582,9 @@ class AllProductData {
     ),
   ];
 
-  List<Produit> get Produits {
+  static List<Produit> get Produits {
     return _produits;
   }
 }
+
+
--- lib/projet_e_commerce/pages/produit_list.dart
+++ lib/projet_e_commerce/pages/produit_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projet1/projet_e_commerce/data/list_produits.dart';
+import 'package:projet1/projet_e_commerce/myWidgets/un_produit.dart';
 
 class ListProduitPage extends StatefulWidget {
   const ListProduitPage({super.key});
@@ -16,7 +18,20 @@ class _MyWidgetState extends State<ListProduitPage> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Text("To DO....."),
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 0.4,
+        children: List.generate(AllProductData.Produits.length, (index) {
+          return InkWell(
+            onTap: () {
+              // 2Méthodes pour transmettre le prodiit vers 
+              // la page Detail_produits
+              // => COmpte rendu
+            },
+            child: WidgetProduit(produit: AllProductData.Produits[index]),
+          );
+        }),
+      ),
     );
   }
 }
